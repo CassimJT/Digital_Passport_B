6cd7895823bb855751bfce89ab865ad11bb2b3f6
Light bekete <bsc-com-ne-07-21@unima.ac.mw>
2025-10-16 01:57:59 +0200

    reformatted res and req parameters and variables intialisation

--- src/utils/smsSender.mjs
+++ src/utils/smsSender.mjs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import twilio from "twilio"
-=======
-//import twilio from twilio
->>>>>>> 4f58d2cd2389b7d2f24f772a9bffc97824f17ece
 
 /*const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
