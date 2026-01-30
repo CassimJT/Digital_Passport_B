a3cd5af6b7cc26b669acab1190b37fef36cb4e8c
CassimJT <cassimjaffer47@gmail.com>
2026-01-30 16:30:51 +0200

    changed to cluster

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -8,7 +8,7 @@ const MONGO_URL_CLASTER  = process.env.MONGO_URL_CLASTER;
 const MONGO_URI_CAMPUSS = process.env.MONGO_URI_CAMPUSS
 const connectDB = async () => {
   try {
-      const conn = await mongoose.connect(MONGO_URI_CAMPUSS);
+      const conn = await mongoose.connect(MONGO_URL_CLASTER);
     
       console.log(`MongoDB Connected: ${conn.connection.host}`);
       //await createSuperUser();
