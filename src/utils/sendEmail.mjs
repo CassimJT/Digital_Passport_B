22e1a622eeb0e2dca5b586a804c73e8be3e63457
CassimJT <cassimjaffer47@gmail.com>
2026-02-02 16:23:24 +0200

    added email title name

--- src/utils/sendEmail.mjs
+++ src/utils/sendEmail.mjs
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, html) => {
   try {
     await resend.emails.send({
-      from: `"My App" <onboarding@resend.dev>`,
+      from: `"Immigration" <onboarding@resend.dev>`,
       to,
       subject,
       html,
