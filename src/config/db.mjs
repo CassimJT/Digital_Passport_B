9ed9aded6d00cf86398915bfa81ab0b34912c26d
CassimJT <cassimjaffer47@gmail.com>
2026-02-05 00:42:15 +0200

    added supper admin

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import User from '../models/User.mjs';
 import { hashPassword } from '../utils/helpers.mjs';
+import Nrb from '../models/Nrb.mjs';
 
 dotenv.config();
 const MONGO_URL_CLASTER  = process.env.MONGO_URL_CLASTER;
@@ -11,7 +12,7 @@ const connectDB = async () => {
       const conn = await mongoose.connect(MONGO_URL_CLASTER || MONGO_URI_CAMPUSS);
     
       console.log(`MongoDB Connected: ${conn.connection.host}`);
-      //await createSuperUser();
+      createSuperUser();
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1); // Exit the app if DB fails
@@ -21,26 +22,39 @@ const connectDB = async () => {
 export default connectDB;
 
 export const createSuperUser = async () => {
+  const superAdminNationalId = process.env.SUPER_ADMIN_NATIONAL_ID;
+  const superAdminEmail = process.env.SUPER_ADMIN_EMAIL;
+  const superAdminPassword = process.env.SUPER_ADMIN_PASSWORD;
   try {
     const existingAdmin = await User.findOne({ role: "superadmin" });
-    const hashedPassword = await hashPassword(process.env.SUPER_ADMIN_PASSWORD)
     if (existingAdmin) {
-      console.log("Already have superadmin");
-      return; 
+      console.log("Super admin already exists");
+      return;
     }
 
+    // Find citizen in NRB by nationalId
+    const nrbCitizen = await Nrb.findOne({
+      nationalId: superAdminNationalId,
+    });
+
+    if (!nrbCitizen) {
+      throw new Error("Super admin citizen not found in NRB database");
+    }
+
+    // Hash password
+    const hashedPassword = await hashPassword(superAdminPassword);
+
+    //Create system user
     const superAdmin = new User({
-      firstname: "immigrationAdmin",
-      lastname: "immigrationAdmin",
-      username: "admin",
-      email: process.env.SUPER_ADMIN_EMAIL,
-      password: hashPassword,
-      role: process.env.SUPER_ADMIN_ROLE || "superadmin"
+      nationalId: nrbCitizen._id,
+      emailAddress: superAdminEmail,
+      password: hashedPassword,
+      role: "superadmin",
     });
 
     await superAdmin.save();
-    console.log("Super admin created");
+    console.log("Super admin account created");
   } catch (error) {
-    console.error(`Failed to add super user: ${error}`);
+    console.error("Failed to create super admin:", error.message);
   }
 };
