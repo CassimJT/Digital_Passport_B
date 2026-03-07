b28e6f644ec867d12e4889c4e6b1fa7ff61831a4
CassimJT <cassimjaffer47@gmail.com>
2026-03-07 09:56:56 +0200

    resolved naming issus on the userController

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -13,7 +13,7 @@ export const getAllUsers = async (req,res,next)=> {
         }
         return res.status(200).json({
             status: "success",
-            message: allUsers
+            data: allUsers
         })
     } catch (error) {
         next(error)
